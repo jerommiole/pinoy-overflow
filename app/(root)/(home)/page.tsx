913e50327b49196e351901c1f1cc49bc9e786f6f
Jerom Miole <miolejerom@gmail.com>
2023-09-28 22:50:20 +1300

    navbar, themes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div></div>;
 }
