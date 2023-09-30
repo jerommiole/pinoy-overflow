ccd0e7cbb62fc7a41cbe908b89141e121e90a073
Jerom Miole <miolejerom@gmail.com>
2023-09-30 17:58:47 +1300

    RightSidebar with RenderTag

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSidebar
+        <RightSidebar />
       </div>
       Toaster
     </main>
