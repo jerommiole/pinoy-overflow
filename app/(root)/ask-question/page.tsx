82f3839c96736cb5da4be315c7e374dcbdc55a46
Jerom Miole <miolejerom@gmail.com>
2023-10-08 20:11:50 +1300

    connect question action in the database

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,24 @@
 import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
+const Page = async () => {
+  // const { userId } = auth();
+
+  const userId = "123456789";
+  if (!userId) redirect("/sign-in");
+
+  const mongoUser = await getUserById({ userId });
+
+  console.log(mongoUser);
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
       <div className="mt-9">
-        <Question />
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
       </div>
     </div>
   );
