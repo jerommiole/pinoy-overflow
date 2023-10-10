c046d7ed927f68f92d70708863966092f666fa3d
Jerom Miole <miolejerom@gmail.com>
2023-10-10 19:26:29 +1300

    add api webhook

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
