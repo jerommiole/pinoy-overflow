7befae1e152e3ef07f3d623754756da59d6269d3
Jerom Miole <miolejerom@gmail.com>
2023-10-19 18:16:47 +1300

    ts ignore

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -150,7 +150,7 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    const { clerkId, searchQuery } = params;
 
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, "i") } }
