5019759347c4c897c75790df91d8fa22d5dd1d14
Jerom Miole <miolejerom@gmail.com>
2023-10-19 18:15:25 +1300

    displaying saved questions

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -16,7 +16,7 @@ interface QuestionProps {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -72,7 +72,7 @@ const QuestionCard = ({
         <Metric
           imgUrl="/assets/icons/like.svg"
           alt="upvotes"
-          value={formatAndDivideNumber(upvotes)}
+          value={formatAndDivideNumber(upvotes.length)}
           title="Votes"
           textStyles="small-medium text-dark400_light800"
         />
