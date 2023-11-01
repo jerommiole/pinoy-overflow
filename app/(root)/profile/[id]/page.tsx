642e98dc4416e8f72b93d732521513a79fdfa44a
jerommiole <miolejerom@gmail.com>
2023-11-02 00:11:04 +1300

    displaying Questions Tab in profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -10,6 +10,7 @@ import { getJoinedDate } from "@/lib/utils";
 import ProfileLink from "@/components/shared/ProfileLink";
 import Stats from "@/components/shared/Stats";
 import QuestionTab from "@/components/shared/QuestionTab";
+import AnswersTab from "@/components/shared/AnswersTab";
 
 const Page = async ({ params, searchParams }: URLProps) => {
   const { userId: clerkId } = auth();
@@ -93,9 +94,15 @@ const Page = async ({ params, searchParams }: URLProps) => {
               </TabsTrigger>
             </TabsList>
             <TabsContent value="top-posts">
-              <QuestionTab />
+              <QuestionTab
+                searchParams={searchParams}
+                userId={userInfo.user._id}
+                clerkId={clerkId}
+              />
             </TabsContent>
-            <TabsContent value="answers">ANSWERS</TabsContent>
+            <TabsContent value="answers">
+              <AnswersTab />
+            </TabsContent>
           </Tabs>
         </Tabs>
       </div>
---
+++ components/shared/AnswersTab.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnswersTab = () => {
+  return <div>AnswersTab</div>;
+};
+
+export default AnswersTab;
