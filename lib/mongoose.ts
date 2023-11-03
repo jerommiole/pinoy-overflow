57940cb72b88008a0967b1124cf12271ea8e91ff
jerommiole <miolejerom@gmail.com>
2023-11-03 16:58:54 +1300

    refactor

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -15,7 +15,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "pinoyoverflow",
+      dbName: "devflow",
     });
     isConnected = true;
     console.log("MongoDB is connected");
