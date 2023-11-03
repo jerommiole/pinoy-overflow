050a8c20df28079deac2fc03875c80d25eefbbdc
jerommiole <miolejerom@gmail.com>
2023-11-03 16:33:25 +1300

    fix the max width in feed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex">
         <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-          <div className="mx-auto w-full max-w-5xl">{children}</div>
+          <div className="mx-auto w-full max-w-3xl">{children}</div>
         </section>
         <RightSidebar />
       </div>
