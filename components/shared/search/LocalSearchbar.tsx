ed227c6c5c3a05188b6a454a02c4d2d2a95ab303
jerommiole <miolejerom@gmail.com>
2023-11-07 23:37:01 +1300

    installing query-string, implementing search state

--- components/shared/search/LocalSearchbar.tsx
+++ components/shared/search/LocalSearchbar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Image from "next/image";
-import React from "react";
-
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 
 interface CustomInputProps {
   route: string;
@@ -20,6 +21,36 @@ const LocalSearchbar = ({
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  const [search, setSearch] = useState(query || "");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "q",
+          value: search,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["q"],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, route, pathname, router, searchParams, query]);
+
   return (
     <div className="relative w-full">
       <div
@@ -37,8 +68,8 @@ const LocalSearchbar = ({
         <Input
           type="text"
           placeholder={placeholder}
-          value=""
-          onChange={() => {}}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
 
