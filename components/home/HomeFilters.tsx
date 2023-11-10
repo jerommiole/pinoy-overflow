34b444d29c38d83cb31b1673e061964c218f2280
jerommiole <miolejerom@gmail.com>
2023-11-10 23:26:25 +1300

    manage state in filter

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,11 +1,36 @@
 "use client";
 
 import { HomePageFilters } from "@/constants/filter";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 flex-wrap gap-3 md:flex">
@@ -18,6 +43,7 @@ const HomeFilters = () => {
               ? "bg-primary-100 text-primary-500"
               : "bg-light-800 text-light-500"
           }`}
+          onClickCapture={() => handleTypeClick(item.value)}
         >
           {item.name}
         </Button>
