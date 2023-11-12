fb640bb68b8126f5fd501b27822a5b60bd431cef
jerommiole <miolejerom@gmail.com>
2023-11-12 22:49:44 +1300

    integrate sort in answer

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -41,11 +41,30 @@ export async function getAnswers(params: GetAnswersParams) {
   try {
     connectToDatabase();
 
-    const { questionId } = params;
+    const { questionId, sortBy } = params;
+
+    let sortOptions = {};
+
+    switch (sortBy) {
+      case "highestUpvotes":
+        sortOptions = { upvotes: -1 };
+        break;
+      case "lowestUpvotes":
+        sortOptions = { upvotes: 1 };
+        break;
+      case "recent":
+        sortOptions = { createAt: -1 };
+        break;
+      case "old":
+        sortOptions = { createAt: 1 };
+        break;
+      default:
+        break;
+    }
 
     const answers = await Answer.find({ question: questionId })
       .populate("author", "_id clerkId name picture")
-      .sort({ createdAt: -1 });
+      .sort(sortOptions);
 
     return { answers };
   } catch (error) {
