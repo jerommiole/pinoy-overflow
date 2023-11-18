f9b8a7a193d7d996c918bde87601ead2d3befbac
jerommiole <miolejerom@gmail.com>
2023-11-18 22:28:26 +1300

    refactor pageSize in collection

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -186,7 +186,7 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, searchQuery, filter, page = 1, pageSize = 2 } = params;
+    const { clerkId, searchQuery, filter, page = 1, pageSize = 10 } = params;
 
     const skipAmount = (page - 1) * pageSize;
 
