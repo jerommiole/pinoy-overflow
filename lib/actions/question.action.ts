cafa05b69bb757b11bc1cbf486b7e0619f16a1e4
jerommiole <miolejerom@gmail.com>
2023-11-20 00:18:15 +1300

    toast implementation

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -21,7 +21,7 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery, filter, page = 1, pageSize = 5 } = params;
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
 
     const skipAmount = (page - 1) * pageSize;
 
